89d7e4a2ae048e08463822b6d0826b526f74ed50
Mohamed Tarek <mohamed82008@gmail.com>
2022-07-08 04:18:37 +1000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/NonconvexNOMAD.jl
@@ -0,0 +1,5 @@
+module NonconvexNOMAD
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NonconvexNOMAD
+using Test
+
+@testset "NonconvexNOMAD.jl" begin
+    # Write your tests here.
+end
